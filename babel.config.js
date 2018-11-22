d523bbfb045063797db4db1e932ae0a445ab955e
Bibek Dahal <bibekdahal.bd16@gmail.com>
2018-11-22 13:24:15 +0545

    Fix eslint configuration

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,12 @@ module.exports = function config(api) {
             '@babel/preset-react',
         ],
         plugins: [
-            '@babel/plugin-proposal-class-properties',
+            [
+                '@babel/plugin-proposal-class-properties',
+                {
+                    loose: true,
+                },
+            ],
             '@babel/plugin-proposal-object-rest-spread',
             [
                 '@babel/plugin-transform-runtime',
